438914523ef3996b21ff879835ba9381fdc12997
YZGGG <2713910440@qq.com>
2019-05-06 19:59:30 +0800

    for ch07

---
+++ ch07/7_4.cpp
@@ -0,0 +1,8 @@
+//Write a class named Person that represents the name and address of a person.Use a string to hold each of these elements. Subsequent exercises will incrementally add features to this class.
+
+class Person
+{
+private:
+	string strName;
+	string strAddress;
+};
